9decc6ca0e876d173f8876ac36fbbcf0553b41bc
yko-git <yko.iwata@gmail.com>
2024-04-05 09:53:16 +0900

    fix develop(関数の配置変更)

--- src/App.js
+++ src/App.js
@@ -1,130 +1,6 @@
 import { useState } from "react";
 import styled from "styled-components";
 
-function Square({ value, onSquareClick }) {
-  return (
-    <>
-      <Cell className="square" onClick={onSquareClick}>
-        {value}
-      </Cell>
-    </>
-  );
-}
-function Board() {
-  const [xIsNext, setxIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Win" + winner;
-  } else {
-    status = "starting";
-  }
-
-  function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
-      return;
-    }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = "○";
-    } else {
-      nextSquares[i] = "×";
-    }
-
-    setSquares(nextSquares);
-    setxIsNext(!xIsNext);
-  }
-
-  return (
-    <>
-      <Items>
-        <Item className={xIsNext ? "isActive" : ""}>○</Item>
-        <Item className={xIsNext ? "" : "isActive"}>×</Item>
-      </Items>
-      <Lboard>
-        <Table>
-          <tbody>
-            <tr className="row">
-              <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-              <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-              <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-            </tr>
-            <tr className="row">
-              <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-              <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-              <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-            </tr>
-            <tr className="row">
-              <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-              <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-              <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-            </tr>
-          </tbody>
-        </Table>
-      </Lboard>
-      <Lfooter>
-        <State>{status}</State>
-        <Reload />
-      </Lfooter>
-    </>
-  );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
-function Reload() {
-  function loadFunc() {
-    window.location.reload();
-  }
-
-  return (
-    <div>
-      <Btn className="Restart" onClick={loadFunc}>
-        RESTART
-      </Btn>
-    </div>
-  );
-}
-
-export default function Game() {
-  return (
-    <>
-      <Html>
-        <Lcontainer>
-          <div>
-            <header>
-              <H1>Tic Tac Toe</H1>
-            </header>
-
-            <Board />
-          </div>
-        </Lcontainer>
-      </Html>
-    </>
-  );
-}
-
 const Html = styled.html`
   background: white;
   font-family: "Lato", "Lucida Grande", "Lucida Sans Unicode", Tahoma,
@@ -202,3 +78,128 @@ const Btn = styled.div`
     cursor: pointer;
   }
 `;
+
+function Square({ value, onSquareClick }) {
+  return (
+    <>
+      <Cell className="square" onClick={onSquareClick}>
+        {value}
+      </Cell>
+    </>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function Reload() {
+  function loadFunc() {
+    window.location.reload();
+  }
+
+  return (
+    <div>
+      <Btn className="Restart" onClick={loadFunc}>
+        RESTART
+      </Btn>
+    </div>
+  );
+}
+
+function Board() {
+  const [xIsNext, setxIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Win" + winner;
+  } else {
+    status = "starting";
+  }
+
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "○";
+    } else {
+      nextSquares[i] = "×";
+    }
+
+    setSquares(nextSquares);
+    setxIsNext(!xIsNext);
+  }
+
+  return (
+    <>
+      <Items>
+        <Item className={xIsNext ? "isActive" : ""}>○</Item>
+        <Item className={xIsNext ? "" : "isActive"}>×</Item>
+      </Items>
+      <Lboard>
+        <Table>
+          <tbody>
+            <tr className="row">
+              <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+              <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+              <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+            </tr>
+            <tr className="row">
+              <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+              <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+              <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+            </tr>
+            <tr className="row">
+              <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+              <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+              <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+            </tr>
+          </tbody>
+        </Table>
+      </Lboard>
+      <Lfooter>
+        <State>{status}</State>
+        <Reload />
+      </Lfooter>
+    </>
+  );
+}
+
+export default function Game() {
+  return (
+    <>
+      <Html>
+        <Lcontainer>
+          <div>
+            <header>
+              <H1>Tic Tac Toe</H1>
+            </header>
+
+            <Board />
+          </div>
+        </Lcontainer>
+      </Html>
+    </>
+  );
+}
